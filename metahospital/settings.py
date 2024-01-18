605231c41a11f3e5594ff0441b3f3686642e3f75
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-01-19 00:45:20 +0600

    database updated

--- metahospital/settings.py
+++ metahospital/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -81,14 +82,21 @@ WSGI_APPLICATION = 'metahospital.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgres://libdata_user:FuqVkJfcJinp0cDeCvpDjMDYwGYABduF@dpg-cmcj65v109ks738vtfbg-a.oregon-postgres.render.com/libdata',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
