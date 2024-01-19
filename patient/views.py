5731c611744033ce069453bf623215aceda85ad8
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-01-19 11:11:46 +0600

    email verification

--- patient/views.py
+++ patient/views.py
@@ -30,7 +30,7 @@ class UserRegistrationViwe(FormView):
         print("token ", token)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         print("uid ", uid)
-        confirm_link = f"http://127.0.0.1:8000/patient/active/{uid}/{token}/"
+        confirm_link = f"https://meta-hospital.onrender.com/patient/active/{uid}/{token}/"
         email_subject = "Confirm Your Email"
         email_body = render_to_string(
             'confirm_email.html', {'confirm_link': confirm_link})
